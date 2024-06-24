d2440377f788c821eb7c884db780708576a28e4e
Marc Alexander Ramirez Aguilar <81877369+AguilarAlex22escom@users.noreply.github.com>
2024-06-24 04:46:11 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
+from config import app as application
 from routes import *
 from errors import *
-
 from api import *
 
-app
+app=application
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    app.run(debug=True)

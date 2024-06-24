19f0d5fc8707f849af63c4f259dfde841dcacb2f
Marc Alexander Ramirez Aguilar <81877369+AguilarAlex22escom@users.noreply.github.com>
2024-06-24 04:58:06 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,8 @@
-from config import app as application
 from routes import *
 from errors import *
 from api import *
 
-app=application
+app=Flask(__name__, template_folder="templates", static_folder="static")
 
 if __name__ == "__main__":
     app.run(debug=True)

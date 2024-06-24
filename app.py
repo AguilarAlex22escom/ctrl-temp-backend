1774b5f34f9f1d5dd9676b3764b138fbdfc55c3d
Marc Alexander Ramirez Aguilar <81877369+AguilarAlex22escom@users.noreply.github.com>
2024-06-24 05:03:29 -0600

    Update and rename __main__.py to app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from config import app as application
 from routes import *
 from errors import *
 from api import *
 
-app=Flask(__name__, template_folder="templates", static_folder="static")
+app=application
 
 if __name__ == "__main__":
     app.run(debug=True)
